0732277c7be213d0864e3f7b71428119d3642362
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-03-10 00:04:27 +0530

    adding color codes

--- contracts/LetsAskNftContract.sol
+++ contracts/LetsAskNftContract.sol
@@ -6,30 +6,60 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-import 'base64-sol/base64.sol';
+import "base64-sol/base64.sol";
 
 contract LetsAskNftContract is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string baseSvg =
-        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    string[] bgColors = ["black", "turquoise", "red", "blue", "brown", "green"];
+    string[] textColors = [
+        "white",
+        "linen",
+        "azure",
+        "bisque",
+        "yellow",
+        "snow"
+    ];
+
+    string svg1 =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: '";
+    string svg2 =
+        "; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svg3 =
+        "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     constructor() ERC721("LetsAsk", "LA") {
         console.log("LetsAskNftContract Constructor");
     }
 
-    function mintLetsAskNft(string memory to, string memory data) public {
-        
+    function mintLetsAskNft(
+        string memory to,
+        string memory data,
+        uint8 textColor,
+        uint8 bgColor
+    ) public {
         require(bytes(data).length > 0, "Empty question to mint");
         require(bytes(data).length > 280, "More than 280 character length");
         require(bytes(to).length > 0, "Empty To Field");
-        require(bytes(to).length > 10, "More than 10 character length of to field");
-
+        require(
+            bytes(to).length > 10,
+            "More than 10 character length of to field"
+        );
+        require(textColor > 6, "Wrong text color value. Should be less than 6");
+        require(bgColor > 6, "Wrong bgColor value. Should be less than 6");
 
         uint256 newItemId = _tokenIds.current();
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, data, "</text></svg>")
+            abi.encodePacked(
+                svg1,
+                textColors[textColor],
+                svg2,
+                bgColors[bgColor],
+                svg3,
+                data,
+                "</text></svg>"
+            )
         );
 
         string memory questionTo = string(
